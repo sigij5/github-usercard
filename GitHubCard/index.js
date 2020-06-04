2896aff99b4518ed671dc902e336b31be1f73f48
Sigi Jaeckel <sigij55@gmail.com>
2020-06-04 15:11:40 -0400

    completed mvp

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -4,6 +4,42 @@
     https://api.github.com/users/<your name>
 */
 
+axios.get('https://api.github.com/users/sigij5')
+  .then(response =>{
+    console.log('response', response)
+    const myInfo = response.data
+    const myCard = cardCreator(myInfo)
+    cards.appendChild(myCard)
+
+    axios.get('https://api.github.com/users/sigij5/following')
+    .then(response =>{
+      console.log('second axios response', response)
+      const following = response.data
+      following.forEach(user =>{
+        followingArray.push(user.login)
+      })
+      followingArray.forEach(user =>{
+        axios.get(`https://api.github.com/users/${user}`)
+          .then(response =>{
+            const userInfo = response.data
+            const userCard = cardCreator(userInfo)
+            cards.appendChild(userCard)
+          })
+          .catch(error =>{
+            console.log('error', error)
+          })
+      })
+    })
+
+  })
+  .catch(error =>{
+    console.log('error', error)
+  })
+
+
+  const cards = document.querySelector('.cards')
+
+  const followingArray =[]
 /*
   STEP 2: Inspect and study the data coming back, this is YOUR
     github info! You will need to understand the structure of this
@@ -28,7 +64,28 @@
     user, and adding that card to the DOM.
 */
 
-const followersArray = [];
+// const followingArray = [
+//   'isaac-gorman',
+//   'JDMTias',
+//   'dhoesle',
+//   'AJKemps',
+//   'JuniorDugue'
+// ];
+
+
+
+followingArray.forEach(user =>{
+  axios.get(`https://api.github.com/users/${user}`)
+    .then(response =>{
+      const userInfo = response.data
+      const userCard = cardCreator(userInfo)
+      cards.appendChild(userCard)
+    })
+    .catch(error =>{
+      console.log('error', error)
+    })
+})
+
 
 /*
   STEP 3: Create a function that accepts a single object as its only argument.
@@ -49,6 +106,52 @@ const followersArray = [];
       </div>
     </div>
 */
+
+function cardCreator(userObj){
+  const card = document.createElement('div')
+  const img = document.createElement('img')
+  const info = document.createElement('div')
+  const name = document.createElement('h3')
+  const username = document.createElement('p')
+  const location = document.createElement('p')
+  const profile = document.createElement('p')
+  const link = document.createElement('a')
+  const followers = document.createElement('p')
+  const following = document.createElement('p')
+  const bio = document.createElement('p')
+
+  card.appendChild(img)
+  card.appendChild(info)
+  info.appendChild(name)
+  info.appendChild(username)
+  info.appendChild(location)
+  info.appendChild(location)
+  info.appendChild(profile)
+  info.appendChild(link)
+  info.appendChild(followers)
+  info.appendChild(following)
+  info.appendChild(bio)
+
+  card.classList.add('card')
+  img.src = userObj.avatar_url
+  info.classList.add('card-info')
+  name.classList.add('name')
+  name.textContent = userObj.name
+  username.classList.add('username')
+  username.textContent = userObj.login
+  location.textContent = `Location: ${userObj.location}`
+  link.href = userObj.html_url
+  link.target = '_blank'
+  link.innerHTML = userObj.html_url
+  profile.textContent = `Profile: `
+  followers.textContent = `Followers: ${userObj.followers}`
+  following.textContent = `Following: ${userObj.following}`
+  bio.textContent = `Bio: ${userObj.bio}`
+
+  return card
+
+}
+
 
 /*
   List of LS Instructors Github username's:
